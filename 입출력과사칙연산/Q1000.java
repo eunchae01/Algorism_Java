391c35e30a6e084f3f4d8c44cd24607e62bf7ce5
eunchae <eunchae01@naver.com>
2023-04-13 14:28:11 +0900

    Q1000, Q1001, Q1008, Q10430, Q10869, Q10926, Q10998, Q18108, Q2557

---
+++ 입출력과사칙연산/Q1000.java
@@ -0,0 +1,25 @@
+//문제
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//출력
+//첫째 줄에 A+B를 출력한다.
+
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class Q1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);   //입력받기 위해 Scanner 클래스 호출
+
+        int A = scanner.nextInt();                  //A값 입력받음
+        int B = scanner.nextInt();                  //B값 입력받음
+
+        System.out.println(A + B);                  //A+B 출력
+
+        scanner.close();                            //사용 끝난 scanner 닫아주기
+    }
+}
