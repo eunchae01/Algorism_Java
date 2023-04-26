3761d5b9308a2ad7f3680fafb1aa2c807f19d773
eunchae <eunchae01@naver.com>
2023-04-26 13:25:14 +0900

    Q10813, Q3052, Q5597

--- 일차원배열/Q10813.java
+++ 일차원배열/Q10813.java
@@ -26,13 +26,30 @@ public class Q10813 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++){
+            arr[i] = i + 1;
+        }
+
         for (int a = 0; a < M; a++){
             st = new StringTokenizer(br.readLine(), " ");
 
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
 
+            int temp;
 
+            temp = arr[i - 1];
+            arr[i - 1] = arr[j - 1];
+            arr[j - 1] = temp;
         }
+
+        for (int i : arr){
+            bw.write(i + " ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
---
+++ 일차원배열/Q5597.java
@@ -0,0 +1,44 @@
+//문제
+//X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+//교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+
+//입력
+//입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
+
+//출력
+//출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+
+package 일차원배열;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q5597 {
+    public static void main(String[] args) throws IOException {
+        Integer[] arr = new Integer[30];
+
+        for (int i = 0; i < 30; i++){
+            arr[i] = i + 1;
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> arrList = new ArrayList<>(Arrays.asList(arr));
+
+        for (int i = 0; i < 28; i++) {
+            arrList.remove(new Integer(br.readLine()));
+        }
+
+        arr = arrList.toArray(new Integer[0]);
+
+        for (int i : arr) {
+            bw.write(String.valueOf(i) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
