3c253e224b5be382926c97ef160a62cddeb860fb
응째 <70687333+eunchae01@users.noreply.github.com>
2023-10-24 09:34:31 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) <= 'z' & my_string.charAt(i) >= 'A'){
+                continue;
+            }else{
+                answer += Integer.valueOf(my_string.charAt(i) - 48);
+            }
+        }
+        
+        return answer;
+    }
+}
