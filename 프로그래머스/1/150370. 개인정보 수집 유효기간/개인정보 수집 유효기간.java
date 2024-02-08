c8a3aef56da37bbc26faf4ace28d2a4b2be80198
응째 <70687333+eunchae01@users.noreply.github.com>
2024-02-08 19:18:23 +0900

    Update 개인정보 수집 유효기간.java

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -4,12 +4,14 @@ class Solution {
     public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
         ArrayList<Integer> answer = new ArrayList<>();
 
+        // . 을 구분자로 년, 월, 일 받아줌
         StringTokenizer st = new StringTokenizer(today, ".");
 
         int year = Integer.parseInt(st.nextToken());
         int month = Integer.parseInt(st.nextToken());
         int day = Integer.parseInt(st.nextToken());
 
+        // hashMap에 약관 종류를 key 유효기간을 value 로 담아줌
         HashMap<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < terms.length; i++) {
@@ -17,8 +19,9 @@ class Solution {
 
             map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
         }
-
+        
         for (int i = 0; i < privacies.length; i++) {
+            // .과 공백문자를 구분자로 각 개인정보의 수집 일자와 약관 종류를 받아줌
             st = new StringTokenizer(privacies[i], ". ");
 
             int befoYear = Integer.parseInt(st.nextToken());
@@ -27,41 +30,36 @@ class Solution {
 
             String befoTerm = st.nextToken();
 
-            if (befoDay == 1){  // 수집 일자가 1일일 경우
-                befoDay = 28;
+            if (befoDay == 1){  // 개인정보 수집 일자가 1일일 경우
+                befoDay = 28;   // 한달에 28일 있다고 가정되었기 때문
 
-                befoMonth += map.get(befoTerm) % 12 - 1;
+                befoMonth += map.get(befoTerm) % 12 - 1;    // 12로 나눈 나머지에서 -1 해준 만큼 더해줌
+            }else {             // 개인정보 수집 일자가 1일이 아닐 경우
+                befoDay--;      // 파기 일자 = 수집 일자 - 1
 
-            }else {         // 수집 일자가 1일이 아닐 경우
-                befoDay--;
-
-                befoMonth += map.get(befoTerm) % 12;
+                befoMonth += map.get(befoTerm) % 12;        // 유효기간 전부 다 더해줌
             }
 
-            befoYear += map.get(befoTerm) / 12;
+            befoYear += map.get(befoTerm) / 12;             // 12로 나눠 나온 몫 만큼 연도에 더해줌
 
-            if (befoMonth > 12){
-                befoMonth -= 12;
-                befoYear++;
+            if (befoMonth > 12){                            // 다 계산한 월이 12 보다 크다면
+                befoMonth -= 12;                            // -12 해주고
+                befoYear++;                                 // 연도++ 해줌
             }
-            
+
+            // 오늘 날짜와 비교하여 유효기간 지난 번호들만 answer에 넣어줌
             if (befoYear < year){
-                //System.out.println("1");
                 answer.add(i + 1);
             }else if(befoYear == year){
                 if (befoMonth < month){
-                    //System.out.println("2");
                     answer.add(i + 1);
                 }else if (befoMonth == month){
                     if (befoDay < day){
-                        //System.out.println("3");
                         answer.add(i + 1);
                     }
                 }
             }
         }
-
-        //System.out.println(answer);
 
         return answer;
     }
