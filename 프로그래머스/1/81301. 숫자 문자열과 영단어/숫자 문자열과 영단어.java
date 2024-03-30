797f9b2a3575f2693582e924d6243e0ba703ca28
응째 <70687333+eunchae01@users.noreply.github.com>
2024-03-31 02:45:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 9.10 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int solution(String s) {
+        String str = "";
+		
+		int index = 0;
+		
+		for (int i = 0; i < s.length(); i = index) {
+			if (Character.isDigit(s.charAt(i))){
+				str += s.charAt(i);
+				index++;
+			}else {
+				if (s.charAt(i) == 'z'){
+					str += '0';
+					index += 4;
+				} else if (s.charAt(i) == 'o') {
+					str += '1';
+					index += 3;
+				} else if (s.charAt(i) == 't') {
+					if (s.charAt(i + 1) == 'w'){
+						str += '2';
+						index += 3;
+					}else {
+						str += '3';
+						index += 5;
+					}
+				} else if (s.charAt(i) == 'f') {
+					if (s.charAt(i + 1) == 'o'){
+						str += '4';
+						index += 4;
+					}else {
+						str += '5';
+						index += 4;
+					}
+				} else if (s.charAt(i) == 's') {
+					if (s.charAt(i + 1) == 'i'){
+						str += '6';
+						index += 3;
+					}else {
+						str += '7';
+						index += 5;
+					}
+				} else if (s.charAt(i) == 'e') {
+					str += '8';
+					index += 5;
+				} else {
+					str += '9';
+					index += 4;
+				}
+			}
+		}
+		
+		return Integer.valueOf(str);
+    }
+}
