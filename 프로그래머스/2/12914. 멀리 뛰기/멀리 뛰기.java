22e863d43faaf7e3525d6d1446778f833fbe30d1
응째 <70687333+eunchae01@users.noreply.github.com>
2024-03-31 00:14:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        int[] jump = new int[n + 1];
+        
+        jump[1] = 1;
+        
+        if(n > 1){
+            jump[2] = 2;
+        }
+        
+        if(n > 2){
+            for(int i = 3; i < jump.length; i++){
+                jump[i] = (jump[i - 1] + jump[i - 2]) % 1234567;
+            }
+
+        }
+        
+        return jump[n];
+    }
+}
