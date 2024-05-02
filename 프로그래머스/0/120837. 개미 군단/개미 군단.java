55a6b566293f5be8526b2ea6465db26ee7d5a503
응째 <70687333+eunchae01@users.noreply.github.com>
2024-05-02 21:31:19 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        while(hp / 3 > 0){
+            if(hp / 5 > 0){
+                answer += (hp / 5);
+                
+                hp %= 5;
+            }else if(hp / 3 > 0){
+                answer += (hp / 3);
+                
+                hp %= 3;
+            }
+        }
+        
+        return answer + hp;
+    }
+}
