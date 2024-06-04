98fec82351b4a509cc7c4807f94082b44da0d8e5
응째 <70687333+eunchae01@users.noreply.github.com>
2024-06-04 16:04:03 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] visit;
+
+    public static void BFS(int computer){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(computer);
+        visit[computer] = true;
+
+        while (!q.isEmpty()){
+            int tmp = q.poll();
+
+            for (int a : list.get(tmp)) {
+                if (!visit[a]){
+                    BFS(a);
+
+                    visit[a] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        visit = new boolean[n + 1];
+        list = new ArrayList<>();
+
+        for (int i = 0; i < n + 1; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        int connect = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < connect; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        BFS(1);
+
+        int answer = 0;
+        for (boolean cnt : visit) {
+            if (cnt){
+                answer++;
+            }
+        }
+
+        System.out.println(answer - 1);
+    }
+}
