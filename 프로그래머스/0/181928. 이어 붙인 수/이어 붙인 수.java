2c859f807f2e301a168d514b6eaf9d7377a7d933
응째 <70687333+eunchae01@users.noreply.github.com>
2024-06-04 16:36:51 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.61 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        String even = "";
+        String odd = "";
+        
+        for(int num : num_list){
+            if(num % 2 == 0){
+                even += String.valueOf(num);
+            }else{
+                odd += String.valueOf(num);
+            }
+        }
+        
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        
+        return answer;
+    }
+}
