e6bbca2dd88d8b2f4ddb27644c4b897601380f30
응째 <70687333+eunchae01@users.noreply.github.com>
2024-07-05 14:21:55 +0900

    [level 2] Title: 하노이의 탑, Time: 5.02 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    ArrayList<int[]> list = new ArrayList<>();
+    
+    public void hanoi(int n, int start, int middle, int end){
+        int[] move = {start, end};
+        
+        if(n == 1){
+            list.add(move);
+        }else {
+            hanoi(n - 1, start, end, middle);
+            
+            list.add(move);
+            
+            hanoi(n - 1, middle, start, end);
+        }
+    }
+    
+    public ArrayList<int[]> solution(int n) {
+        hanoi(n, 1, 2, 3);
+        
+        return list;
+    }
+}
